9153cf4e16ec41446576afe0d842f630a88b7084
Rashi Abramson <roshkins@gmail.com>
2017-04-01 13:27:11 -0700

    Added response times

--- app/assets/javascripts/help_sources.js
+++ app/assets/javascripts/help_sources.js
@@ -1,3 +1,13 @@
-function checkResponseTime() {
-  $.ajax({url: "https://data.marincounty.org/resource/7bzr-ymkc.json?$query=SELECT MAX(time_call_was_received) as max_arrival_time, MAX(time_arrived_on_scene) AS max_call_time, incident_zip_postal AS zip  GROUP BY zip"})
+function checkResponseTime(zip) {
+  $.ajax({
+    url: "https://data.marincounty.org/resource/7bzr-ymkc.json?$query=SELECT time_call_was_received as call_time, time_arrived_on_scene AS arrive_time, incident_zip_postal AS zip  WHERE zip=\""+zip+"\"",
+    dataType: "json",
+    success: function (data) {
+      var max = data.reduce((acc, val) => {
+        var diff = (new Date(val.arrive_time)).getMinutes() - (new Date(val.call_time)).getMinutes();
+        return ((new Date(val.arrive_time)).getFullYear() == 2016) ? Math.max(diff, acc) : acc;
+      }, 0);
+      $("#time").text(max);
+    }
+  });
 }
